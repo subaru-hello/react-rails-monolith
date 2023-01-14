17460eae65031f51845e204b0a2f9968fbee7dca
subaru-hello <octosubaru0926@gmail.com>
2023-01-14 12:36:41 +0900

    add initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+author = Author.create(name: 'subaru')
+
+author.posts.create(title: '日記', content: '今日も一日頑張った')
