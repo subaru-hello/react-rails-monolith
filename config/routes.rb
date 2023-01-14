2d53a9e1e8552104e6267680cf712f5963249ef6
subaru-hello <octosubaru0926@gmail.com>
2023-01-14 21:57:40 +0900

    remove tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts, only: %i[index]
+      resources :authors, only: %i[show]
     end
   end
  root "home#index"
