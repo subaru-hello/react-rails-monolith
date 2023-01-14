99e66b5e4af03e6d8913ef425e9b2d20c65dc559
subaru-hello <octosubaru0926@gmail.com>
2023-01-14 12:43:03 +0900

    add posts endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: %i[index]
+    end
+  end
  root "home#index"
+ get "*path", to: "home#index"
 end
